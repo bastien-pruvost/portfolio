c90d292b0b679f98dc6db39ec6893a20c096f0df
Bastien Pruvost <bastien.mory@gmail.com>
2023-02-23 23:28:16 +0100

    feat: add custom font on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
       colors: {
         current: 'currentColor',
         grey: {
