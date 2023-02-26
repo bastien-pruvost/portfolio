bad65ed70166e63668a77d8225f38f3b462533a6
Bastien Pruvost <bastien.mory@gmail.com>
2023-02-26 19:21:54 +0100

    refactor: remove head.tsx and add metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import '@/assets/styles/globals.css'
 
+export const metadata = {
+  title: 'Portfolio Bastien Pruvost | Développeur Web',
+  description: 'This is my insane portfolio',
+}
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang='fr'>
+    <html lang='fr' suppressHydrationWarning>
       <head />
       <body>{children}</body>
     </html>
