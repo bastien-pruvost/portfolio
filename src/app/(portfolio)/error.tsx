aec0d913545c2ab1c2bd401856b9a28f3c3d0ad3
Bastien Pruvost <bastien.mory@gmail.com>
2023-02-28 17:07:40 +0100

    feat: add nextjs error handler

---
+++ src/app/(portfolio)/error.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { cn } from '@/lib/utils/classname'
+import { useEffect } from 'react'
+
+export type ErrorProps = {
+  error: Error
+  reset: () => void
+}
+
+const Error = ({ error, reset }: ErrorProps) => {
+  useEffect(() => {
+    console.error({ error })
+  }, [error])
+
+  return (
+    <div className='z-50 mt-4 text-center'>
+      <p className={cn('mb-4 text-xl font-medium text-red-800 opacity-100 dark:text-red-300')}>
+        Une erreur est survenue !
+      </p>
+      <button
+        className={cn('rounded-lg bg-red-900 px-4 py-2 text-white shadow-lg')}
+        onClick={() => reset()}
+      >
+        Réessayer
+      </button>
+    </div>
+  )
+}
+
+export default Error
