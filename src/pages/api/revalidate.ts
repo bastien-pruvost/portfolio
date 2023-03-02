a9c40daff52109c7123be33867cf6610a1b0cc0c
Bastien Pruvost <bastien.mory@gmail.com>
2023-03-02 12:17:21 +0100

    feat: add on demand revalidation end point in api

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  console.log(request.query.token)
+
+  if (request.query.token !== process.env.REVALIDATION_TOKEN) {
+    return response.status(401).json({ message: 'Token invalide' })
+  }
+
+  try {
+    await response.revalidate('/profile')
+    return response.status(200).json({
+      success: true,
+      revalidated: true,
+      message: 'Revalidation des données éfféctuée avec succés',
+    })
+  } catch (error) {
+    return response.status(500).json({
+      success: false,
+      message: 'Une erreur est survenu durant la revalidation des données.',
+      error,
+    })
+  }
+}
+
+export default handler
