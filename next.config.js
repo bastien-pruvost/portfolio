71b62275f55b4f4799543b668b8eb25968190a24
Bastien Pruvost <bastien.mory@gmail.com>
2023-03-07 13:03:57 +0100

    fix: remove typed routes (error when building)

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
     optimizeCss: true,
-    typedRoutes: true,
+    // typedRoutes: true,
   },
   redirects() {
     return [{ source: '/', destination: '/profile', permanent: true }]
